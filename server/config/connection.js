5b7cf91cdea2d0acee9fbed46009850d318e9b37
Tamara Sferco <122523521+TamaraDawg@users.noreply.github.com>
2023-07-25 20:43:22 +1000

    Database seeds

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/events_db');
+const dbUrl = 'mongodb://localhost:27017/events_db';
+mongoose.connect(process.env.MONGODB_URI || dbUrl);
 
 module.exports = mongoose.connection;
+module.exports = dbUrl;
